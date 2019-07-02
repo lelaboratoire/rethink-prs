7901b6d8bd6fb2882c8849a330a3c3e5f40f33e6
lelaboratoire <grixor@gmail.com>
2019-07-02 14:57:23 -0400

    split data into train and test

--- mb-mdr/reformat-data.R
+++ mb-mdr/reformat-data.R
@@ -5,13 +5,23 @@
 library(dplyr)
 library(data.table)
 
+set.seed(1618)
+
 mdr_path <- here::here('mb-mdr/')
 data_dir <- here::here('simulated-data')
 filenames <- list.files(data_dir, pattern = '*.txt')
 
 for (filename in filenames){
-  paste(data_dir, filename, sep = '/') %>%
+  mydat <- paste(data_dir, filename, sep = '/') %>%
     fread() %>%
-    dplyr::select(Class, paste0('X', 0:9)) %>%
-    fwrite(paste0(mdr_path, 'reformatted-data/', filename), sep = '\t')
+    dplyr::select(Class, paste0('X', 0:9)) 
+  nsamp <- nrow(mydat)
+  
+  train_idx <- sample(seq(nsamp), floor(nsamp/2), replace = F)
+  train_dat <- mydat[train_idx, ]
+  test_dat <- mydat[-train_idx, ]
+  
+  # fwrite(mydat, paste0(mdr_path, 'reformatted-data/', filename), sep = '\t')
+  fwrite(train_dat, paste0(mdr_path, 'reformatted-data/train/', filename), sep = '\t')
+  fwrite(test_dat, paste0(mdr_path, 'reformatted-data/test/', filename), sep = '\t')
 }
