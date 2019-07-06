c4db9c32d81f24c85c13b27ced53399a1fecfd1b
Trang Le <grixor@gmail.com>
2019-07-05 21:00:48 -0400

    add utility functions

---
+++ analysis/utils.R
@@ -0,0 +1,69 @@
+## Define some utility functions
+
+sub_name <- function(filetype, my_file){
+  gsub('.txt', glue::glue('_{filetype}.txt'), my_file)
+}
+
+read_risks <- function(filetype, my_file){
+  fread(here('mb-mdr', 'risks', sub_name(filetype, filename)))
+}
+
+my_t_test <- function(var){
+  t.test(x = cases[, var], y = ctrls[, var], 'greater', paired = TRUE) %>%
+    broom::tidy() %>%
+    select(statistic, p.value)
+}
+
+risktype_recode <- function(prefix = 'prs', x){
+  fct_recode(x, 
+             Original = paste0(prefix, '_ori'),
+             `MM1` = paste0(prefix, '_1d'), 
+             `MM2` = paste0(prefix, '_2d'), 
+             `MM3` = paste0(prefix, '_3d'),
+             `MM12` = paste0(prefix, '_12d'))
+}
+
+# risktype_recode_roc <- function(x){
+#   fct_recode(x, 
+#              Original = 'rocs_ori',
+#              `MM1` = 'rocs_1d', 
+#              `MM2` = 'rocs_2d',
+#              `MM3` = 'rocs_3d',
+#              `MM3` = 'rocs_3d')
+# }
+
+roc_func <- function(risk_type){
+  PRROC::roc.curve(scores.class0 = all_risks[, risk_type],
+                   weights.class0 = all_risks$Class,
+                   curve = T)
+}
+
+pr_func <- function(risk_type){
+  tryCatch(
+    PRROC::pr.curve(scores.class0 = all_risks[, risk_type],
+                    weights.class0 = all_risks$Class,
+                    curve = T),
+    error = function(c) NA)
+}
+
+get_auroc <- function(x, risktype){
+  tryCatch(x[[risktype]]$auc,
+           error = function(c) NA)
+}
+
+get_auprc <- function(x, risktype){
+  tryCatch(x[[risktype]]$auc.integral,
+           error = function(c) NA)
+}
+
+get_roc_curve <- function(var, varname){
+  df <- as.data.frame(roci[[var]]$curve)
+  df$type <- varname
+  df
+}
+
+get_pr_curve <- function(var, varname){
+  df <- as.data.frame(pri[[var]]$curve)
+  df$type <- varname
+  df
+}
