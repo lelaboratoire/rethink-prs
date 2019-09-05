9af4cd223f133ccf9847698ccb1e77790f9167cf
Trang Le <grixor@gmail.com>
2019-09-05 18:21:34 -0400

    run mb-mdr on real data

---
+++ mb-mdr/2-run-mb-mdr-real.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fname="reformatted-data/real-train/small_Glaucoma_results_snps_pval0.01_6PCs_prune0.6_wPheno_covariates.txt"
+# run mbmdr on real data file
+echo "Processing $fname file..."
+for dimension in 1D 2D; do
+  ./mbmdr-4.4.1-mac-64bits.out \
+    --binary \
+    -ac 8 \
+    -d $dimension \
+    -o "${fname%.*}_output_$dimension.txt" \
+    -o2 "${fname%.*}_model_$dimension.txt" \
+    "$fname"
+done
