e24fc1ed6661f8e4c3a11d49038dab8d23b572a2
lelaboratoire <grixor@gmail.com>
2019-09-10 15:10:14 -0400

    run mbmdr on entire real train set

--- mb-mdr/2-run-mb-mdr-real.sh
+++ mb-mdr/2-run-mb-mdr-real.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fname="reformatted-data/real-train/small_Glaucoma_results_snps_pval0.01_6PCs_prune0.6_wPheno_covariates.txt"
+fname="reformatted-data/real-train/Glaucoma_results_snps_pval0.01_6PCs_prune0.6_wPheno_covariates.txt"
 # run mbmdr on real data file
 echo "Processing $fname file..."
 for dimension in 1D 2D; do
